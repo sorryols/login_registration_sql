4e65f5c526d6977dc08d8ec8f5c9113c305af9b5
sorryols <106112015+sorryols@users.noreply.github.com>
2023-03-21 17:19:10 +0200

    add project file

---
+++ src/main/java/com/example/login_registration_sql/For_work_with_sql/Constant.java
@@ -0,0 +1,9 @@
+package com.example.login_registration_sql.For_work_with_sql;
+
+public class Constant {
+    public static final String USER_TABLE = "users";
+    public static final String USERs_ID = "idusers";
+    public static final String USER_NAME = "user_name";
+    public static final String USER_PASS = "user_pass";
+    public static final String USER_MONEY = "user_money";
+}
