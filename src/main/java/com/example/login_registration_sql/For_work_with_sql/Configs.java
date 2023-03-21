89785d81588865d48ce7367e4fd536d1425b47ac
sorryols <106112015+sorryols@users.noreply.github.com>
2023-03-21 17:20:10 +0200

    add project file

---
+++ src/main/java/com/example/login_registration_sql/For_work_with_sql/Configs.java
@@ -0,0 +1,9 @@
+package com.example.login_registration_sql.For_work_with_sql;
+
+public class Configs {
+    protected String dbHost ="localhost";
+    protected String dbPort ="3306";
+    protected String dbUser ="root";
+    protected String dbPass ="root";
+    protected String dbName ="ivan";
+}
